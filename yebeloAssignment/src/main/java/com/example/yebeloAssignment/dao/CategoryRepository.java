94fb6e93174b3722af3f1d76f74cac6fc8de06d3
Asharudheen MT <mtasharudheen@gmail.com>
2022-06-26 17:07:00 +0530

    added repository interface CategoryRepository.java which extends the CrudRepository

---
+++ yebeloAssignment/src/main/java/com/example/yebeloAssignment/dao/CategoryRepository.java
@@ -0,0 +1,17 @@
+package com.example.yebeloAssignment.dao;
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.yebeloAssignment.entity.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+	Category findByCategoryCode(int id);
+	
+	List <Category> findAll();
+}
