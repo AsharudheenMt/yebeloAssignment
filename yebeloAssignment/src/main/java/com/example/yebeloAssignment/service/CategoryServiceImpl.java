b74b3359f6951e8aa9d46f58c8467e41798db006
Asharudheen MT <mtasharudheen@gmail.com>
2022-06-26 17:25:13 +0530

    added the service/CategoryServiceImpl.java with dependency injection of repository and added method to find the sum of digits.

--- yebeloAssignment/src/main/java/com/example/yebeloAssignment/service/CategoryServiceImpl.java
+++ yebeloAssignment/src/main/java/com/example/yebeloAssignment/service/CategoryServiceImpl.java
@@ -2,12 +2,40 @@ package com.example.yebeloAssignment.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.yebeloAssignment.dao.CategoryRepository;
 import com.example.yebeloAssignment.entity.Category;
 
 @Service
-public class CategoryServiceImpl implements CategoryService{
+public class CategoryServiceImpl implements CategoryService {
+
+	private CategoryRepository categoryRepository;
+
+	// constructor injection of categoryRepository
+	@Autowired
+	public CategoryServiceImpl(CategoryRepository categoryRepository) {
+		this.categoryRepository = categoryRepository;
+	}
+
+//	Method for finding the sum of individual digits
+	static int digSum(int n) {
+		int sum = 0;
+
+		// Loop to do sum while
+		// sum is not less than
+		// or equal to 9
+		while (n > 0 || sum > 9) {
+			if (n == 0) {
+				n = sum;
+				sum = 0;
+			}
+			sum += n % 10;
+			n /= 10;
+		}
+		return sum;
+	}
 
 	@Override
 	public Category getCategoryByCategoryCode(int categoryCode) {
@@ -30,8 +58,7 @@ public class CategoryServiceImpl implements CategoryService{
 	@Override
 	public void saveOrUpdate(Category category) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
 
 }
